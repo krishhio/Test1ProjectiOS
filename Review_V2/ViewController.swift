ccf7f1729708fb30e84b0257d0aaf7e8bd14e8f8
Cristian Gomez <crishhio@gmail.com>
2020-06-17 23:39:20 -0500

    Initial Commit

---
+++ Review_V2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Review_V2
+//
+//  Created by Cristian Gómez on 17/06/20.
+//  Copyright © 2020 Cristian Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
